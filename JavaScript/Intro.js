477a6f95a20c9a06741e8a0afc6379743ae05bad
Shikhar Khandelwal <skykunnu@gmail.com>
2024-08-07 11:52:15 +0530

    new

---
+++ JavaScript/Intro.js
@@ -0,0 +1,11 @@
+// Three ways of displaying output in js. 
+
+// document.write('Welcome to JS'); // print the output on screen
+
+// console.log('Welcome to js'); // print the output on console.
+
+// alert('Welcome to js'); // print the output as a pop-up.
+
+
+// Prompt are used to take value from the user.
+ 
