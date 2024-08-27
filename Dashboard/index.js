1a54322537d5f75d838af2dc20136d46060691cf
Shikhar Khandelwal <skykunnu@gmail.com>
2024-08-27 15:54:25 +0530

    new

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -5,5 +5,6 @@ let innerbox1=document.getElementById('innerbox1');
 homeIcon.addEventListener("click",myFunction)
 
 function myFunction(){
-        innerbox1.style.backgroundColor="white";
+        innerbox1.style.backgroundColor="#c7cfd9";
+        innerbox1.style.color='black';
 }
