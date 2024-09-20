efc8617f22870cd02960aac2db14afa53a80b80a
Shikhar Khandelwal <skykunnu@gmail.com>
2024-09-20 10:23:20 +0530

    new

---
+++ Leaderboard/leaderboard.js
@@ -0,0 +1,9 @@
+let fName=document.getElementById('fName');
+let lName=document.getElementById('lName');
+let Country=document.getElementById('Country');
+let Score=document.getElementById('Score');
+
+
+function player(){
+    
+}
