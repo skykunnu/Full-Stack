5ef5133ae1a89a16be1190168e54cd8414719a8d
Shikhar Khandelwal <skykunnu@gmail.com>
2024-09-20 16:35:29 +0530

    new

--- Leaderboard/leaderboard.js
+++ Leaderboard/leaderboard.js
@@ -5,5 +5,19 @@ let Score=document.getElementById('Score');
 
 
 function player(){
+    let playerRow=document.createElement('tr');
+    let fNd=document.createElement('td');
+    fNd.innerHTML=fName.value;
+    let lNd=document.createElement('td');
+    lNd.innerHTML=lName.value;
+    let country=document.createElement('td');
+    country.innerHTML=Country.innerHTML;
+    let score=document.createElement('td');
+    score.innerHTML=Score.value;
     
+    DownTable.append(playerRow);
+    DownTable.append(fNd);
+    DownTable.append(lNd);
+    DownTable.append(country);
+    DownTable.append(score);
 }
