dff6180ff6b963f2ded1268687808c878ac68b73
Shikhar Khandelwal <skykunnu@gmail.com>
2024-09-20 18:34:27 +0530

    new

--- Leaderboard/leaderboard.js
+++ Leaderboard/leaderboard.js
@@ -15,9 +15,30 @@ function player(){
     let score=document.createElement('td');
     score.innerHTML=Score.value;
     
+    playerRow.append(fNd);
+    playerRow.append(lNd);
+    playerRow.append(country);
+    playerRow.append(score);
+    
+    let DeleteBtn=document.createElement('button');
+    DeleteBtn.innerHTML="D";
+    playerRow.append(DeleteBtn);
+
+     DeleteBtn.onclick=function(){
+       DownTable.removeChild(playerRow)
+     }
+    let addFive=document.createElement('button');
+    addFive.innerHTML='+5';
+    playerRow.append(addFive);
+    addFive.onclick=function(){
+        score.innerHTML=Number(score.innerHTML)+Number(5);
+      }
+    let subFive=document.createElement('button');
+    subFive.innerHTML='-5';
+    playerRow.append(subFive);
+    subFive.onclick=function(){
+        score.innerHTML=Number(score.innerHTML)-Number(5);
+      }
+    
     DownTable.append(playerRow);
-    DownTable.append(fNd);
-    DownTable.append(lNd);
-    DownTable.append(country);
-    DownTable.append(score);
 }
