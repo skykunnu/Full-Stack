d31233a9fb9631ede195c245720e00f0dd538d6d
Shikhar Khandelwal <skykunnu@gmail.com>
2024-09-23 21:46:11 +0530

    new

---
+++ JavaScript/DOM/DOM.md
@@ -0,0 +1,24 @@
+1. 
+
+<select id=""> -> In case of select always give id to select tag and value to option tag to work in DOM. 
+and give one empty value of one option tag and give disabled selected to work as a placeholder for select. 
+<option value=""></option>
+<option value=""></option>
+<option value=""></option>
+<option value=""></option>
+<option value=""></option>
+</select>
+
+
+2. If we want to create a table using js in DOM 
+<div class="table">
+<table id="DownTable">
+               
+</table>
+</div>
+
+now create tr and under tr create td as much as you want. 
+first append all td inside tr and then append tr inside for ex DownTable.
+
+
+3. 
--- Leaderboard/leaderboard.js
+++ Leaderboard/leaderboard.js
@@ -2,43 +2,97 @@ let fName=document.getElementById('fName');
 let lName=document.getElementById('lName');
 let Country=document.getElementById('Country');
 let Score=document.getElementById('Score');
+let downTable=document.getElementById('DownTable');
 
 
 function player(){
+  
+
+  if(fName.value || lName.value && Country.value || Score.value){
     let playerRow=document.createElement('tr');
+
+
     let fNd=document.createElement('td');
     fNd.innerHTML=fName.value;
+
+
     let lNd=document.createElement('td');
     lNd.innerHTML=lName.value;
+
+
     let country=document.createElement('td');
-    country.innerHTML=Country.innerHTML;
+    country.innerHTML=Country.value;
+
+
     let score=document.createElement('td');
     score.innerHTML=Score.value;
     
-    playerRow.append(fNd);
-    playerRow.append(lNd);
-    playerRow.append(country);
-    playerRow.append(score);
+    playerRow.append(fNd,lNd,country,score);
+
+
     
     let DeleteBtn=document.createElement('button');
     DeleteBtn.innerHTML="D";
-    playerRow.append(DeleteBtn);
 
-     DeleteBtn.onclick=function(){
-       DownTable.removeChild(playerRow)
-     }
     let addFive=document.createElement('button');
     addFive.innerHTML='+5';
-    playerRow.append(addFive);
-    addFive.onclick=function(){
-        score.innerHTML=Number(score.innerHTML)+Number(5);
-      }
+
     let subFive=document.createElement('button');
     subFive.innerHTML='-5';
-    playerRow.append(subFive);
+
+    playerRow.append(DeleteBtn,addFive,subFive);
+
+
+
+     DeleteBtn.onclick=function(){
+       downTable.removeChild(playerRow) // removes playerRow(Child element) from downTable(parent element).
+       sortPlayers();
+      }
+
+
+
+    addFive.onclick=function(){
+        score.innerHTML=Number(score.innerHTML)+Number(5);
+        sortPlayers();
+      }
+
+
+
+
     subFive.onclick=function(){
         score.innerHTML=Number(score.innerHTML)-Number(5);
+        sortPlayers();
       }
     
-    DownTable.append(playerRow);
+
+
+    downTable.append(playerRow);
+
+    fName.value=""
+    lName.value=""
+    Country.value=""
+    Score.value=""
+
+
+sortPlayers();
+
+  }
+else{
+  alert("Please Enter value's of all the fields");
 }
+
+
+}
+
+
+
+function sortPlayers() {
+  let PlayerRow = Array.from(downTable.getElementsByTagName('tr'));
+  PlayerRow.sort((A, B) => {
+      let scoreA = Number(A.children[3].innerHTML);
+      let scoreB = Number(B.children[3].innerHTML);
+      return scoreB - scoreA; 
+  });
+  PlayerRow.forEach(player => downTable.append(player));
+}
+
