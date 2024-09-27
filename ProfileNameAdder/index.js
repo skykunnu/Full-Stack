2740885f5fdba1b32c9682b419c12f40cbfa3e55
Shikhar Khandelwal <skykunnu@gmail.com>
2024-09-27 17:24:51 +0530

    ProfileNameAdder

--- ProfileNameAdder/index.js
+++ ProfileNameAdder/index.js
@@ -3,8 +3,12 @@ let ProfileAdd=document.querySelector("#profileAdd");
 let cancel=document.querySelector('#cancel_1');
 let confirm=document.querySelector('#confirm_1');
 let cross1=document.querySelector('#cross_1');
+let cross2=document.querySelector('#cross_2');
 let Profiles=document.querySelector("#profiles");
 let Name=document.querySelector('#EnterName');
+let ProfileDel=document.querySelector('#profileDelete');
+let Cancel_2=document.querySelector('#cancel_2');
+let Confirm_2=document.querySelector('#confirm_2');
 
 
 addBtn.addEventListener('click',function(){
@@ -14,18 +18,47 @@ addBtn.addEventListener('click',function(){
 
 confirm.addEventListener('click',function(){
    
-    if(Name.value=="" || typeof(Name.value)!=String){
+    if(Name.value=="" && typeof(Name.value)!=String){
         alert('Please enter Name properly!')
     }
     
     else{
         let person=document.createElement('button');
+        let Span=document.createElement('span');
+        let I=document.createElement('i');
+        Span.id='cross_3';
+        I.classList='fa-regular fa-circle-xmark';
         person.classList='Persons';
         person.innerHTML=Name.value[0];
         person.style.textTransform='capitalize'
-        Profiles.append(person)
+        Span.append(I);
+        person.append(Span);
+        Profiles.append(person);
         ProfileAdd.style.display='None';
         Name.value=""
+
+
+        Span.addEventListener('click',function(){
+            ProfileDel.style.display='block';
+
+
+            Cancel_2.addEventListener('click',function(){
+                ProfileDel.style.display='None';
+        
+               })
+        
+               Confirm_2.addEventListener('click',function(){
+                ProfileDel.style.display='None';
+                Profiles.removeChild(person);
+        
+               })
+
+
+       })
+
+
+      
+
     }
 
 })
@@ -40,3 +73,9 @@ cross1.addEventListener('click',function(){
     ProfileAdd.style.display='None';
     
 })
+
+cross2.addEventListener('click',function(){
+    ProfileDel.style.display='None';
+    
+})
+
