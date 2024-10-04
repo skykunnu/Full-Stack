1d368631cf90e2b5b283af5c16db7d64012d2a51
Shikhar Khandelwal <skykunnu@gmail.com>
2024-10-04 18:15:12 +0530

    new

--- DrumKIt/drum.js
+++ DrumKIt/drum.js
@@ -3,13 +3,12 @@ let ImgItems=document.querySelector('#imgItems');
 ImgItems.addEventListener('click',drumSet);
 
 function drumSet(e){
-    if(e.target.nodeName==='IMG'){ // It is targeting drum Items Image
+    if(e.target.nodeName==='IMG'){ // It is targeting drum Items Image (ie <img>)
         playDrumItems(e.target.id); // It is targerting id associated to drum Items Image ex- Crash, Kick, Snare, Tom. 
     }
 }
 
 function playDrumItems(drumItem){
-    let audio= new Audio(`${drumItem}.mp3`);
+    let audio= new Audio(`${drumItem}.mp3`); // It is way of creating
     audio.play();
-    // audio.pause();
 }
