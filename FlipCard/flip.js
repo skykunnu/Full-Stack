7e85940b77cb5d8cf0351f72e8f7b4278aa6e9bc
Shikhar Khandelwal <skykunnu@gmail.com>
2024-10-14 18:13:38 +0530

    new

--- FlipCard/flip.js
+++ FlipCard/flip.js
@@ -1,40 +1,74 @@
-let FlipCard=document.querySelectorAll('.flip-card');
-let RandomImages=[
-    { src: 'Audi.jpg' },
-    { src: 'Bentley.jpg' },
-    { src: 'BMW.png' },
-    { src: 'Ferrari.jpg' },
-    { src: 'Mercedes.jpg' },
-    { src: 'Porsche.jpg' }
-    
-]
+let ImagesArr=['Audi.jpg','Bentley.jpg','BMW.png','Ferrari.jpg','Mercedes.jpg','Porsche.jpg']
+let flipFrontImage=document.querySelectorAll('.flip-card-front img');
+let actualImage=[...ImagesArr, ...ImagesArr];
+let tempImagesArr=[];
+let flipBackImage=document.querySelectorAll('.flip-card-back');
+let openImagesStore=[];
+let openImages=0;
+let result=document.querySelector("#Result");
+let Container=document.querySelector('.container');
+let timer=0;
 
 
-
-FlipCard.forEach(flip_Card=>{
-    flip_Card.addEventListener('click',function(){
-        let flipCardInner=flip_Card.querySelector('.flip-card-inner')
-        let flipCardBack=flip_Card.querySelector(".flip-card-back");
-        flipCardBack.innerHTML=""
-    
-        let Img=document.createElement('img');
-    
-    let randomIndex = Math.floor(Math.random() * RandomImages.length);
-    
-    Img.src = RandomImages[randomIndex].src;
-
-
-    flipCardBack.append(Img)
-    flipCardInner.style.transform='rotateY(180deg)'
-
-
-    setTimeout(()=>{
-    flipCardInner.style.transform='rotateY(0deg)'
+backImage();
+startTimer();
+flipFrontImage.forEach((flipFront_image)=>{
+    flipFront_image.addEventListener('click',()=>{
+        openImages++;
+        flipFront_image.parentElement.parentElement.classList.add('backSide');
+        openImagesStore.push(flipFront_image.parentElement.nextElementSibling.children[0])
+        
+        setTimeout(()=>{
+            if(openImages>1){
+                if(openImagesStore[0].src===openImagesStore[1].src){
+                    openImages=0;
+                openImagesStore.length=0;
+                
+            }
+            else{
+                openImagesStore.forEach((Img)=>{
+                    Img.parentElement.parentElement.classList.remove('backSide');
+                })
+                openImages=0;
+                openImagesStore.length=0;
+            }
+        }
+        
     },1000)
-
-
-
-
-    })
+})
 })
 
+
+function startTimer(){
+   let interval=setInterval(()=>{
+        timer++;
+       if(timer===60){
+        Container.style.display='none';
+        result.style.display='block';
+        clearInterval(interval);
+       }
+       console.log(timer)
+    },900)
+}
+
+
+
+
+
+function randomIndex(){
+    let RandomIndex=Math.floor(Math.random()*actualImage.length);
+    if(tempImagesArr.includes(RandomIndex)) return randomIndex();
+    else{
+        tempImagesArr.push(RandomIndex);
+        return RandomIndex;
+    }
+}
+
+function backImage(){
+    for(let i=0;i<actualImage.length;i++){
+
+        let Img=document.createElement('img');
+        Img.src=actualImage[randomIndex()];
+        flipBackImage[i].append(Img);
+    }
+}
