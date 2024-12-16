76d0da481787b29a5c90a669c0b1ae81a76d3a3c
Shikhar Khandelwal <skykunnu@gmail.com>
2024-12-16 22:56:20 +0530

    Keyboard event

---
+++ JavaScript/DOM/KeyboardEvents/script.js
@@ -0,0 +1,18 @@
+// Types of Keyboard events
+// Keyboard event can be applied on any element. 
+/*
+1. keyUp
+2. keyDown
+3. keyPress
+*/
+
+// const h1=document.querySelector('h1')
+// const input=document.querySelector('input')
+
+// As input is a focus element but h1 is not then how to make it focusable element. 
+// will use an attribute tabindex and assign 0 to make h1 focusable.
+
+// window.addEventListener('keypress', (e)=>{
+//     console.log('Code:',e.code)
+//     console.log('Value:',e.key) // It will gives the keys pressed inside input element. 
+// })
