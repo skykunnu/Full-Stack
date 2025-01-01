c9c158a50bebb7f0b45e757b11aa44a68f9b8637
Shikhar Khandelwal <skykunnu@gmail.com>
2025-01-01 15:12:52 +0530

    new addtion

---
+++ DSA_JS/Array.js
@@ -0,0 +1,6 @@
+const arr=[1,2,3,'vishwas'];
+// console.log(arr[0])
+
+for(const item of arr){
+    console.log(item)
+}
---
+++ JavaScript/Sets.js
@@ -0,0 +1,22 @@
+// set constructor-> the constructor optionaly accepts array as an argument. 
+
+const set=new Set([1,2,3]);
+
+set.add(4) // to add a new value. 
+
+console.log(set.has(4)) // It tells whether the element is present or not by returning boolean value. 
+
+// set.delete(3) // deletes the element from the set. 
+
+console.log(set.size) // returns the size of the set.
+
+set.clear() // It removes all values from a set. 
+ 
+for(const item of set){
+    console.log(item) 
+}
+
+
+
+
+
