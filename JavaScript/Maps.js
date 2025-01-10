085622516bd1654177973730bd1725590752d553
Shikhar Khandelwal <skykunnu@gmail.com>
2025-01-10 10:54:05 +0530

    changeOfName

--- JavaScript/Maps.js
+++
@@ -1,9 +0,0 @@
-// The constructor function optionally accepts an array as an argument. 
-
-const map= new Map([['a',1],['b',2]])
-
-map.set('c',3) // this is a set method not a data structure that sets value in an array.  
-
-for(const [key,value] of map){
-   console.log(`${key} : ${value}`);
-}
